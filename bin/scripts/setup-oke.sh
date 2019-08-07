04f82af94bf47be214417a475d8e071ceeca7894
Joseph Che <j_weiming@hotmail.com>
2019-08-07 12:53:42 +0800

    Update setup-oke.sh

--- bin/scripts/setup-oke.sh
+++ bin/scripts/setup-oke.sh
@@ -3,7 +3,7 @@
 
 echo "**** Start of OKE Setup ****"
 echo "**** Generating SSH Key ****"
-mkdir oke-keys
+mkdir ./bin/scripts/terraform/oke-keys
 ssh-keygen -t rsa -b 2048 -f ./bin/scripts/terraform/oke-keys/oke_rsa_keys -N "" > ./bin/scripts/terraform/oke-keys/oke_rsa_keys.output
 
 echo "**** Cloning OCI-OKE Terraform ****"
